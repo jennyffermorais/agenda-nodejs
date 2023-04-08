64ceb492f7a12abe096525e3b053303b65d064a1
jennyffermorais <jennyffernovaes@hotmail.com>
2023-04-08 20:11:42 -0300

    Added user login functionality

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,13 +1,15 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-    res.render('login')
+    // console.log(req.session.user);
+    if (req.session.user) return res.render('login-logado');
+    return res.render('login');
 }
 
-exports.register = async (req, res) => {
+exports.login = async (req, res) => {
     try {
         const login = new Login(req.body)
-        await login.register();
+        await login.login();
 
         if (login.errors.length > 0) {
             req.flash('errors', login.errors);
@@ -17,7 +19,8 @@ exports.register = async (req, res) => {
             return;
         }
 
-        req.flash('success', 'Seu usuário foi criado com sucesso');
+        req.flash('success', 'Login realizado com  sucesso.');
+        req.session.user = login.user;
         req.session.save(function () {
             return res.redirect('back');
         });
@@ -27,4 +30,34 @@ exports.register = async (req, res) => {
         console.log(e);
         return res.render('403');
     }
+}
+
+// exports.register = async (req, res) => {
+//     try {
+//         const login = new Login(req.body)
+//         await login.register();
+
+//         if (login.errors.length > 0) {
+//             req.flash('errors', login.errors);
+//             req.session.save(function () {
+//                 return res.redirect('back');
+//             });
+//             return;
+//         }
+
+//         req.flash('success', 'Seu usuário foi criado com sucesso');
+//         req.session.save(function () {
+//             return res.redirect('back');
+//         });
+
+//         //return res.send(login.errors);
+//     } catch (e) {
+//         console.log(e);
+//         return res.render('403');
+//     }
+// }
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 }
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -4,6 +4,7 @@ exports.middlewareGlobal = (req, res, next) => {
   // está disponível em todas as páginas
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
   next();
 };
 
